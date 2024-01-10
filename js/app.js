b4c61f9d6f975f5cea8523ff75c53752dc5091bb
cyamao <yamao.cuzou@ynov.com>
2024-01-10 09:46:22 +0100

    [ADD] new Direction artistique for the website Extranet ynov

--- js/app.js
+++ js/app.js
@@ -93,8 +93,7 @@ $(function () {
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                     'searchreplace wordcount visualblocks visualchars code fullscreen',
                     'insertdatetime media nonbreaking save table contextmenu directionality',
-                    'emoticons template paste textcolor colorpicker textpattern imagetools'
-                ],
+                    'emoticons template paste textcolor colorpicker textpattern imagetools'],
                 toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
                 toolbar2: 'forecolor backcolor | preview code',
                 image_advtab: true,
